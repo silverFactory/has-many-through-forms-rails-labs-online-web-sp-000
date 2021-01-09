c62a280780a9a1b31608d0847b97761b3b6ab89e
Will Studabaker <will.studabaker@gmail.com>
2021-01-09 10:42:16 -0500

    commit tree 2029524acd3467c14f54bed3987e27c64ac6e266

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,14 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  def user_attributes=(user_attributes)
+    user_attributes.values.each do |user_attribute|
+    #  binding.pry
+      if user_attribute != ""
+        user = User.find_or_create_by(username: user_attribute)
+        self.user = user
+        self.save
+      end
+    end
+  end
 end
